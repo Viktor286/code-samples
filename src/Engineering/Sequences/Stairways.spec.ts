455169d83ec6ecbb201ee17a691e8cf5051b35aa
Viktor286 <just.viktor@gmail.com>
2020-02-29 16:26:48 -0500

    Stairways problem p2

--- src/Engineering/Sequences/Stairways.spec.ts
+++ src/Engineering/Sequences/Stairways.spec.ts
@@ -1,9 +1,11 @@
 import {
   numOfTwoWaysDeclarativeRecursion,
   numOfTwoWaysIterativeMemo,
+  numOfWaysRecursion,
+  numOfWaysMemo,
 } from './Stairways';
 
-const expectedSequenceForTwo: number[] = [1, 1, 2, 3, 5, 8];
+const expectedSequenceForTwo: number[] = [1, 1, 2, 3, 5, 8, 13, 21];
 
 describe('numOfTwoWaysDeclarativeRecursion', () => {
   it('should return result according to expectedSequenceForTwo', () => {
@@ -17,6 +19,24 @@ describe('numOfTwoWaysIterativeMemo', () => {
   it('should return result according to numOfTwoWaysIterativeMemo', () => {
     expectedSequenceForTwo.forEach((answer, idx) => {
       expect(numOfTwoWaysIterativeMemo(idx)).toEqual(answer);
+    });
+  });
+});
+
+describe('numOfWaysRecursion', () => {
+  it('should return result according to numOfTwoWaysIterativeMemo', () => {
+    const allowedSteps = [1, 2];
+    expectedSequenceForTwo.forEach((answer, idx) => {
+      expect(numOfWaysRecursion(idx, allowedSteps)).toEqual(answer);
+    });
+  });
+});
+
+describe('numOfWaysMemo', () => {
+  it('should return result according to numOfTwoWaysIterativeMemo', () => {
+    const allowedSteps = [1, 2];
+    expectedSequenceForTwo.forEach((answer, idx) => {
+      expect(numOfWaysMemo(idx, allowedSteps)).toEqual(answer);
     });
   });
 });
--- src/Engineering/Sequences/Stairways.ts
+++ src/Engineering/Sequences/Stairways.ts
@@ -1,6 +1,7 @@
 // For the N of stairs find number of all possible sequences of steps to climb to the top.
-// First version of the problem for 1 or 2 stairs per step
 // Example of problem could be found at https://www.youtube.com/watch?v=5o-kdjv7FD0
+
+// ----- First version of the problem for 1 or 2 stairs per step ----- //
 
 export function numOfTwoWaysDeclarativeRecursion(n: number): number {
   // base case for situations on last stairs
@@ -20,4 +21,29 @@ export function numOfTwoWaysIterativeMemo(n: number): number {
     sols[s] = sols[s - 1] + sols[s - 2];
   }
   return sols[sols.length - 1];
+}
+
+// ----- Unified solution for different set of steps ----- //
+
+export function numOfWaysRecursion(n: number, allowedSteps: number[]): number {
+  if (n === 0) return 1;
+  let intermediateSum = 0;
+  allowedSteps.forEach(stepLen => {
+    if (n - stepLen >= 0) {
+      intermediateSum += numOfWaysRecursion(n - stepLen, allowedSteps);
+    }
+  });
+  return intermediateSum;
+}
+
+export function numOfWaysMemo(n: number, allowedSteps: number[]) {
+  const sols = [1];
+  for (let step = 1; step <= n; step++) {
+    let intermediateSum = 0;
+    allowedSteps.forEach(stepLen => {
+      if (step - stepLen >= 0) intermediateSum += sols[step - stepLen];
+    });
+    sols[step] = intermediateSum;
+  }
+  return sols[n];
 }
