02ed3924aa618ecb8b82abb5873df4575178b53b
Viktor286 <just.viktor@gmail.com>
2021-02-16 18:16:46 -0500

    permutationsLexicgOrder start

---
+++ src/Engineering/Combinatorics/Permutations_Lexicg_Order.ts
@@ -0,0 +1,22 @@
+// Implementation examples:
+// Java: Heap's algorithm, Recursive, Iterative, Randomized
+// https://www.techiedelight.com/std_next_permutation-overview-implementation/
+// https://www.baeldung.com/java-array-permutations#:~:text=Permutations%20of%20an%20Array%20in%20Java%201%20Introduction,look%20at%20is%20Heap%27s%20algorithm.%20...%204%20Conclusion
+// https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+// https://www.techiedelight.com/generate-permutations-string-java-recursive-iterative/
+//
+// "Permutation Generation Methods" ROBERT SEDGEWlCK, 1977
+// http://homepage.divms.uiowa.edu/~goodman/22m150.dir/2007/Permutation%20Generation%20Methods.pdf
+//
+// Lexicographically previous permutation (looks like it not work properly)
+// https://www.geeksforgeeks.org/lexicographically-previous-permutation-in-c/
+//
+// Iterative approach to find permutations of a string in C++, Java and Python
+// https://www.techiedelight.com/find-permutations-string-cpp-java-iterative/
+//
+// Lexicographically previous permutation With One swap
+// https://algorithms.tutorialhorizon.com/lexicographically-previous-permutation-with-one-swap/?share=facebook
+
+export function permutationsLexicgOrder(l: number[]): number {
+  return 0;
+}
