1901611c41abb9859be6e6d30202610fa56013a4
viktor286 <just.viktor@gmail.com>
2022-03-06 10:22:24 -0500

    add 3 code samples

---
+++ src/Engineering/Arrays/66_Plus_One.ts
@@ -0,0 +1,16 @@
+// You are given a large integer represented as an integer array digits.
+// Increment the integer by one and return the resulting array of digits.
+// [4,3,2,1] -> [4,3,2,2]
+
+const plusOne = function(digits: number[]): number[] {
+  for (let i= digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+
+    digits[i] = 0;
+  }
+
+  return [1, ...digits];
+};
