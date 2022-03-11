13a570c6d8e66b5237626035f2b86212a7548c3d
viktor286 <just.viktor@gmail.com>
2022-03-10 21:59:53 -0500

    add 941 comments

--- src/Engineering/Arrays/941_valid-mountain-array.ts
+++ src/Engineering/Arrays/941_valid-mountain-array.ts
@@ -1,3 +1,11 @@
+// https://leetcode.com/problems/valid-mountain-array/
+// Given an array of integers arr, return true if it is a valid mountain array.
+// arr is a mountain array if and only if:
+// arr.length >= 3
+// There exists some i with 0 < i < arr.length - 1 such that:
+// arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+// arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
 const validMountainArray = function(arr: number[]) {
   let l = 0;
   let r = arr.length - 1;
