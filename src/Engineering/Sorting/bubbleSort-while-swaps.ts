a27f53a0341bac61c38a8c30ef347355ff914922
viktor286 <just.viktor@gmail.com>
2022-03-15 11:42:34 -0400

    add two bubble sorts

---
+++ src/Engineering/Sorting/bubbleSort-while-swaps.ts
@@ -0,0 +1,25 @@
+// Wikipedia: https://en.wikipedia.org/wiki/Bubble_sort
+// Animated Visual: https://www.toptal.com/developers/sorting-algorithms/bubble-sort
+function bubbleSortWhileSwaps(arr: number[]) {
+  let swapped = true;
+  let rightBound = arr.length - 1;
+
+  while (swapped) {
+    swapped = false;
+
+    for (let i = 0; i < rightBound; i++) {
+      if (arr[i] > arr[i + 1]) {
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+
+    if (swapped) {
+      rightBound--;
+    }
+  }
+
+  return arr;
+}
---
+++ src/Engineering/Sorting/bubbleSort.ts
@@ -0,0 +1,22 @@
+// Wikipedia: https://en.wikipedia.org/wiki/Bubble_sort
+// Animated Visual: https://www.toptal.com/developers/sorting-algorithms/bubble-sort
+function bubbleSort(arr: number[]) {
+  for (let i = 0; i < arr.length; i++) { // ->[...]
+    let swapped = false;
+
+    for (let j = arr.length - 1; j >= i; j--) { // -> [<-]
+      if (arr[j - 1] > arr[j]) {
+        const temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+        swapped = true;
+      }
+    }
+
+    if (!swapped) {
+      break;
+    }
+  }
+
+  return arr;
+}
