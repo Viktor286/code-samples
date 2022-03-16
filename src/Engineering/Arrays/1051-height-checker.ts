bc12383cbd93c71320b7bd8d644762e0a5498d74
viktor286 <just.viktor@gmail.com>
2022-03-15 21:07:50 -0400

    fix 1051-height-checker

--- src/Engineering/Arrays/1051-height-checker.ts
+++ src/Engineering/Arrays/1051-height-checker.ts
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/height-checker/
 // Which elements are out of order compared to the positions if every person was ordered correctly?
-// if we compare sorted = [1, 1, 1, 2, 3, 4] to the original input = [1, 1, 4, 2, 1, 3], we notice that sorted[i] == input[i] for i = 0, 1, 3 and hence the remaining elements in indexes 2, 4, 5 are out of place. So the answer is 3.
+// if we compare sorted = [1, 1, 1, 2, 3, 4] to the original input = [1, 1, 4, 2, 1, 3],
+// we notice that sorted[i] == input[i] for i = 0, 1, 3 and hence
+// the remaining elements in indexes 2, 4, 5 are out of place. So the answer is 3.
 
 function heightChecker(heights: number[]) {
   const hCount = new Array(101);
