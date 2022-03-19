dc3f6a5964ff8eda6f164d851e3d354a2e3849c4
viktor286 <just.viktor@gmail.com>
2022-03-19 11:29:12 -0400

    add 414-448

---
+++ src/Engineering/Arrays/414-third-maximum-number.ts
@@ -0,0 +1,38 @@
+// Given an integer array nums, return the third distinct maximum number in this array.
+// If the third maximum does not exist, return the maximum number.
+// Input: nums = [3,2,1]
+// Output: 1
+
+function thirdMax(nums: number[]) {
+  let firstMax = -Infinity;
+  let secondMax = -Infinity;
+  let thirdMax = -Infinity;
+
+  for(let i=0; i<nums.length; i++) {
+    const current = nums[i];
+
+    if (
+      current === firstMax ||
+      current === secondMax ||
+      current === thirdMax
+    ) {
+      continue;
+    }
+
+    if (current > firstMax) {
+      thirdMax = secondMax;
+      secondMax = firstMax;
+      firstMax = current;
+    } else if (current > secondMax) {
+      thirdMax = secondMax;
+      secondMax = current;
+    } else if (current > thirdMax) {
+      thirdMax = current;
+    }
+  }
+
+  if (thirdMax !== -Infinity)
+    return thirdMax;
+
+  return firstMax;
+}
