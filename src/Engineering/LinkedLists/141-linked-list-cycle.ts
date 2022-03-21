aeb2f6f7bc8faba85ed9f211394559c384d1dc8b
viktor286 <just.viktor@gmail.com>
2022-03-21 19:04:14 -0400

    add 141-linked-list-cycle

---
+++ src/Engineering/LinkedLists/141-linked-list-cycle.ts
@@ -0,0 +1,29 @@
+// 141. Linked List Cycle
+// https://leetcode.com/problems/linked-list-cycle/
+// Given head, the head of a linked list,
+// determine if the linked list has a cycle in it.
+
+interface ListNode {
+  val: number,
+  next: ListNode | null
+}
+
+function hasCycle(head: ListNode) {
+  if (!head) {
+    return false;
+  }
+
+  let walker = head;
+  let runner = head;
+
+  while(runner.next !== null && runner.next.next !== null) {
+    walker = walker.next;
+    runner = runner.next.next;
+
+    if (walker === runner) {
+      return true;
+    }
+  }
+
+  return false;
+}
