5aedbc290fa73b44929a75cdc73e91f9117aa540
viktor286 <just.viktor@gmail.com>
2022-04-02 15:07:01 -0400

    add 226

---
+++ src/Engineering/Arrays/226-invert-binary-tree-iter.ts
@@ -0,0 +1,30 @@
+// 226. Invert Binary Tree
+// https://leetcode.com/problems/invert-binary-tree/
+// Given the root of a binary tree, invert the tree,
+// and return its root.
+
+interface BiNode {
+  left: BiNode,
+  right: BiNode,
+  val: number
+}
+
+function invertTree(root: BiNode) {
+  if (!root) {
+    return null;
+  }
+
+  const queue = [root];
+  while(queue.length > 0) {
+    const current = queue.shift();
+
+    const left = current.left;
+    current.left = current.right;
+    current.right = left;
+
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+
+  return root;
+}
---
+++ src/Engineering/Arrays/226-invert-binary-tree-recur.ts
@@ -0,0 +1,25 @@
+// 226. Invert Binary Tree
+// https://leetcode.com/problems/invert-binary-tree/
+// Given the root of a binary tree, invert the tree,
+// and return its root.
+
+interface BiNode {
+  left: BiNode,
+  right: BiNode,
+  val: number
+}
+
+function invertTreeRecursive(root: BiNode) {
+  if (!root) {
+    return null;
+  }
+
+  const left = root.left;
+  root.left = root.right;
+  root.right = left;
+
+  invertTreeRecursive(root.left);
+  invertTreeRecursive(root.right);
+
+  return root;
+}
