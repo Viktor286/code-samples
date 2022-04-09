0324550db0a90ffa3183024fb7a67dc3a33897ec
viktor286 <just.viktor@gmail.com>
2022-04-09 16:18:17 -0400

    add alt edge detection

---
+++ src/Engineering/LinkedLists/141-linked-list-cycle-backedge.ts
@@ -0,0 +1,29 @@
+// 141. Linked List Cycle
+// https://leetcode.com/problems/linked-list-cycle/
+// Given head, the head of a linked list,
+// determine if the linked list has a cycle in it.
+
+interface ListNode {
+  val: number,
+  next: ListNode | null
+  visited: boolean
+}
+
+function hasCycle(head: ListNode) {
+  if (!head) {
+    return false;
+  }
+
+  let current = head;
+
+  while(current) {
+    if (current.visited) {
+      return true;
+    }
+
+    current.visited = true;
+    current = current.next
+  }
+
+  return false;
+}
