f096907569d0fbf95be85583f24ef5fbdcb855f8
viktor286 <just.viktor@gmail.com>
2022-04-16 14:48:38 -0400

    add 102

---
+++ src/Engineering/BinaryTrees/102-binary-tree-level-order-traversal.ts
@@ -0,0 +1,34 @@
+// 102. Binary Tree Level Order Traversal
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+// Given the root of a binary tree, return the level order traversal
+// of its nodes' values. (i.e., from left to right, level by level).
+
+interface IBinaryNode {
+  left: IBinaryNode | null;
+  right: IBinaryNode | null;
+  val: number;
+}
+
+function levelOrder(root: IBinaryNode): number[][] {
+  const collection: number[][] = [];
+
+  if (!root) {
+    return collection;
+  }
+
+  let queue = [root];
+
+  while (queue.length > 0) {
+    const level = [...queue];
+    collection.push(level.map(e => e.val));
+
+    level.forEach(node => {
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    });
+
+    queue = queue.slice(level.length);
+  }
+
+  return collection;
+}
