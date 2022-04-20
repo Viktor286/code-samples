64c2eddabaa8432632a08520d4c58905b092ae86
viktor286 <just.viktor@gmail.com>
2022-04-20 13:20:36 -0400

    add promiseAllExtended

--- src/Javascript_Specifics/Async/PromiseAll_Implemented.ts
+++ src/Javascript_Specifics/Async/PromiseAll_Implemented.ts
@@ -19,3 +19,35 @@ export function promiseAll(promisesArr: Promise<any>[]): Promise<any[]> {
     });
   });
 }
+
+export function promiseAllExtended(promisesArr: Promise<any>[]): Promise<any[]> {
+  return new Promise((resolve, reject) => {
+    if (promisesArr.length === 0) {
+      resolve([]);
+      return;
+    }
+
+    const outputCollection: any[] = [];
+
+    promisesArr.forEach((p, i) => {
+      if (p instanceof Promise) {
+        p.then(
+          (res) => {
+            outputCollection[i] = res;
+            if (outputCollection.length === promisesArr.length) {
+              resolve(outputCollection);
+            }
+          },
+          (err) => {
+            reject(err);
+          },
+        );
+      } else {
+        outputCollection[i] = p;
+        if (outputCollection.length === promisesArr.length) {
+          resolve(outputCollection);
+        }
+      }
+    });
+  });
+}
