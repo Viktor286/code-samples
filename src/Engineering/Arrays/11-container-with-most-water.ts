73fda2345868f197da8f44f9c73dfb92c1938d41
viktor286 <just.viktor@gmail.com>
2022-04-20 21:25:04 -0400

    add 11

---
+++ src/Engineering/Arrays/11-container-with-most-water.ts
@@ -0,0 +1,22 @@
+// 11. Container With Most Water
+// https://leetcode.com/problems/container-with-most-water/
+// Find two lines that together with the x-axis form a container, such that the container contains the most water.
+// Return the maximum amount of water a container can store.
+
+function maxArea(A: number[]): number {
+  let l = 0;
+  let r = A.length - 1;
+  let maxWater = 0;
+
+  while(l < r) {
+    maxWater = Math.max(maxWater, Math.min(A[l], A[r]) * (r - l));
+
+    if (A[l] > A[r]) {
+      r--;
+    } else {
+      l++;
+    }
+  }
+
+  return maxWater;
+}
