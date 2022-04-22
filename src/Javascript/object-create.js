e40d43ea239831ee45ba2e52976f474c5025a898
viktor286 <just.viktor@gmail.com>
2022-04-21 22:17:39 -0400

    add myObjectCreate

--- src/Javascript/object-create.js
+++ src/Javascript/object-create.js
@@ -2,7 +2,16 @@
  * @param {any} proto
  * @return {object}
  */
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/setPrototypeOf
 function myObjectCreate(proto) {
+  if (proto === null || typeof proto !== 'object') throw new TypeError();
+  const o = {};
+  Object.setPrototypeOf(o, proto);
+  return o;
+}
+
+function _myObjectCreate(proto) {
   if (proto === null || proto === undefined || typeof proto !== 'object') throw Error;
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
