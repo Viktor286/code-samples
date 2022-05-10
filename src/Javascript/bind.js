9cbb1d4bb2f2bc8e13669be12f250af0028aef34
viktor286 <just.viktor@gmail.com>
2022-05-10 12:49:18 -0400

    add one more memoize and upd bind

--- src/Javascript/bind.js
+++ src/Javascript/bind.js
@@ -1,8 +1,7 @@
-Function.prototype.bind = function (context) {
-  // eslint-disable-next-line @typescript-eslint/no-this-alias
-  const fn = this;
-  return (...arguments) => fn.call(context, ...arguments);
-};
+// const myTest = test.bind({}, 'applied args')
+Function.prototype.bind = function(ctx, ...bindArgs) {
+  return (...args) => this.call(ctx, ...bindArgs, ...args);
+}
 
 function test(b, c) {
   console.log(this.a);
@@ -11,5 +10,9 @@ function test(b, c) {
 }
 
 const fn = test.bind({ a: 'Hello ' });
+const fn2 = test.bind({ a: 'Goodbye ' }, 555);
+const fn3 = test.bind({ a: 'Bonjour ' }, 777, 888);
 
 fn(1, 2);
+fn2(1, 2);
+fn3(1, 2);
---
+++ src/Javascript/memo-3.js
@@ -0,0 +1,17 @@
+// memoize with accessible cache
+
+function memoize(fn) {
+  function memoFn(...args) {
+    const key = JSON.stringify(args);
+    if (memoFn.cache[key]) {
+      console.log('FROM CACHE');
+      return memoFn.cache[key];
+    }
+    console.log('FRESH COMPUTE')
+    memoFn.cache[key] = fn(...args);
+    return memoFn.cache[key];
+  }
+
+  memoFn.cache = Object.create(null);
+  return memoFn;
+}
