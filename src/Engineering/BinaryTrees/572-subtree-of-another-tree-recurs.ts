bde1a252e0000056cf1c15c51818c3a4180c8e3a
viktor286 <just.viktor@gmail.com>
2022-05-16 15:22:05 -0400

    upd 572

--- src/Engineering/BinaryTrees/572-subtree-of-another-tree-recurs.ts
+++ src/Engineering/BinaryTrees/572-subtree-of-another-tree-recurs.ts
@@ -9,15 +9,15 @@ interface IBinaryNode {
   val: number;
 }
 
+function isSubtree(root: IBinaryNode, subRoot: IBinaryNode): boolean {
+  if (!root) return false;
+  if (isSame(root, subRoot)) return true;
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+}
+
 function isSame(s: IBinaryNode, t: IBinaryNode): boolean {
   if (!s && !t) return true;
   if (!s || !t) return false;
   if (s.val !== t.val) return false;
   return isSame(s.left, t.left) && isSame(s.right, t.right);
-}
-
-function isSubtree(root: IBinaryNode, subRoot: IBinaryNode): boolean {
-  if (!root) return false;
-  if (isSame(root, subRoot)) return true;
-  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
 }
