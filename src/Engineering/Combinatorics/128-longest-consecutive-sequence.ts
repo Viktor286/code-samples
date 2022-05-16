9e00d90758aa623f38b46436c3e50d89b5ea66a0
viktor286 <just.viktor@gmail.com>
2022-05-16 12:07:37 -0400

    upd 128

--- src/Engineering/Combinatorics/128-longest-consecutive-sequence.ts
+++ src/Engineering/Combinatorics/128-longest-consecutive-sequence.ts
@@ -2,29 +2,6 @@
 // https://leetcode.com/problems/longest-consecutive-sequence/
 // Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 
-// BF
-function longestConsecutiveBF(nums: number[]) {
-  let longestStreak = 0;
-
-  for (let n = 0; n < nums.length; n++) {
-    let currentNum = nums[n];
-    let currentStreak = 1;
-
-    for (let i = 0; i < nums.length; i++) {
-      for (let j = 0; j < nums.length; j++) {
-        if (nums[j] === currentNum + 1) {
-          currentStreak++;
-          currentNum++;
-        }
-      }
-    }
-
-    longestStreak = Math.max(currentStreak, longestStreak);
-  }
-
-  return longestStreak;
-}
-
 // BF optimized with memo and sequence limitation
 function longestConsecutive(nums: number[]) {
   const set = new Set();
