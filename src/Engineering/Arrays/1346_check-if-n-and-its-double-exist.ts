20d995b1c1af18d6d50313c12ad81559c7d7864c
viktor286 <just.viktor@gmail.com>
2022-05-18 15:56:22 -0400

    upd 1346

--- src/Engineering/Arrays/1346_check-if-n-and-its-double-exist.ts
+++ src/Engineering/Arrays/1346_check-if-n-and-its-double-exist.ts
@@ -2,13 +2,13 @@
 // N and M such that N is the double of M ( i.e. N = 2 * M; M = N / 2).
 
 function checkIfExist(arr: number[]): boolean {
-  const seen = new Map();
+  const seen = new Set();
 
   for (let i = 0; i < arr.length; i++) {
     if (seen.has(2 * arr[i]) || (arr[i] % 2 === 0 && seen.has(arr[i] / 2))) {
       return true;
     }
-    seen.set(arr[i], 1);
+    seen.add(arr[i]);
   }
 
   return false;
