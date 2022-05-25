76c691885da8250f4c78e9351429fa44be28f160
viktor286 <just.viktor@gmail.com>
2022-05-24 23:32:57 -0400

    upd+ 704

--- src/Engineering/Arrays/704-binary-search.ts
+++ src/Engineering/Arrays/704-binary-search.ts
@@ -16,7 +16,7 @@ function binarySearch(nums: number[], target: number) {
       return mid;
     }
 
-    if (nums[mid] > target) {
+    if (target < nums[mid]) {
       right = mid - 1;
     } else {
       left = mid + 1;
