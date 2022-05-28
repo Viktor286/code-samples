ba4b46efb0b52dd2599571dac9da7904218b7035
viktor286 <just.viktor@gmail.com>
2022-05-28 14:46:52 -0400

    upd 347

--- src/Engineering/Arrays/347-top-k-frequent-elements.ts
+++ src/Engineering/Arrays/347-top-k-frequent-elements.ts
@@ -17,26 +17,28 @@ function topKFrequent(nums: number[], k: number) {
     }
   }
 
-  // build bucket sort for elements in the frequency map
-  const priorityToNumsArr = new Array(nums.length);
-
-  // priority = index = frequency - 1
+  // build an ordered storage like "bucket sort",
+  // where the "key" will be: priority = frequency - 1 = index
+  // and the "value" will be the "number element" itself.
+  // Lets call the key of the map "PFI" as a reminder that
+  // key represents Priority, Frequency and Index at the same time.
+  const pfiToNum = new Array(nums.length);
   numToFreq.forEach((frequency, number) => {
-    if (!priorityToNumsArr[frequency - 1]) {
-      priorityToNumsArr[frequency - 1] = [number];
+    if (!pfiToNum[frequency - 1]) {
+      pfiToNum[frequency - 1] = [number];
     } else {
-      priorityToNumsArr[frequency - 1].push(number);
+      pfiToNum[frequency - 1].push(number);
     }
   });
 
-  // gather the results
+  // gather the results from the end of the array
   const topKFrequent = [];
-  for (let d = priorityToNumsArr.length - 1; d >= 0 && k > 0; d--) {
-    if (priorityToNumsArr[d] !== undefined) {
+  for (let d = pfiToNum.length - 1; d >= 0 && k > 0; d--) {
+    if (pfiToNum[d] !== undefined) {
       // handle collisions
-      topKFrequent.push(priorityToNumsArr[d].pop());
+      topKFrequent.push(pfiToNum[d].pop());
       // stay on the same index if array still not empty
-      if (priorityToNumsArr[d].length > 0) d++;
+      if (pfiToNum[d].length > 0) d++;
       k--;
     }
   }
