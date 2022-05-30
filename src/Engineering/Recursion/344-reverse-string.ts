a52e42b7fee3b316d79d3a0bb36af5f069844d45
viktor286 <just.viktor@gmail.com>
2022-05-30 16:23:27 -0400

    add

---
+++ src/Engineering/Recursion/344-reverse-string.ts
@@ -0,0 +1,20 @@
+// 344. Reverse String
+// https://leetcode.com/problems/reverse-string/
+// Write a function that reverses a string. (in-place)
+// input is a char array: ["h","e","l","l","o"]
+
+function swap(s: string[], a: number, b: number) {
+  const temp = s[a];
+  s[a] = s[b];
+  s[b] = temp;
+}
+
+function reverse(s: string[], l: number, r: number) {
+  if (l >= r) return;
+  swap(s, l, r); // move pointers, then start recursion
+  reverse(s, ++l, --r);
+}
+
+function reverseString(s: string[]) {
+  reverse(s, 0, s.length - 1);
+}
