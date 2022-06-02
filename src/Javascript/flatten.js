da14ed658212a7fea67d17648837f2410b8bd58b
viktor286 <just.viktor@gmail.com>
2022-06-02 12:01:01 -0400

    upd flatten

--- src/Javascript/flatten.js
+++ src/Javascript/flatten.js
@@ -1,6 +1,8 @@
 /**
  * @param {any[]} arr
  */
+const test = [1, 2, [3, 4, [5, 6], 7, 8], 9];
+
 function flatten(arr) {
   return arr.reduce((acc, next) => {
     if (Array.isArray(next)) return acc.concat(flatten(next));
@@ -8,5 +10,18 @@ function flatten(arr) {
   }, []);
 }
 
-const test = [1, 2, [3, 4, [5, 6], 7, 8], 9];
-console.log(flatten(test)); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+function flattenSimple(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!Array.isArray(arr[i])) {
+      result.push(arr[i]);
+    } else {
+      result = result.concat(flatten(arr[i]));
+    }
+  }
+
+  return result;
+}
+
+console.log(JSON.stringify(flatten(test)) === "[1,2,3,4,5,6,7,8,9]");
