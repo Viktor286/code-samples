576201fa3ffc9453aad422db7158618b16f066fc
viktor286 <just.viktor@gmail.com>
2022-06-02 15:52:12 -0400

    add custom useReducer

---
+++ src/Javascript/react-custom-useReducer.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function useReducer(reducer, initialState) {
+  const [state, setState] = useState(initialState);
+
+  function dispatch(action) {
+    const nextState = reducer(state, action);
+    setState(nextState);
+  }
+
+  return [state, dispatch];
+}
+
+function todosReducer(state, action) {
+  switch (action.type) {
+    case 'add':
+      return [...state, {
+        text: action.text,
+        completed: false
+      }];
+    // ... other actions ...
+    default:
+      return state;
+  }
+}
+
+function Todos() {
+  const [todos, dispatch] = useReducer(todosReducer, []);
+
+  function handleAddClick(text) {
+    dispatch({ type: 'add', text });
+  }
+}
