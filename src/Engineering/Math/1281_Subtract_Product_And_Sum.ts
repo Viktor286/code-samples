d5d328fcc317051d7b63986f5710475f141fcb4e
viktor286 <just.viktor@gmail.com>
2022-06-29 10:55:15 -0400

    adj 1281

--- src/Engineering/Math/1281_Subtract_Product_And_Sum.ts
+++ src/Engineering/Math/1281_Subtract_Product_And_Sum.ts
@@ -1,6 +1,7 @@
 // 1281. Subtract the Product and Sum of Digits of an Integer
 // https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
-// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+// Given an integer number n, return the difference between
+// the product of its digits and the sum of its digits.
 
 const subtractProductAndSum = function (n: number): number {
   let prod = 1;
