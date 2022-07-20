405491303bf0092b2325985bcea46139e9da8d98
viktor286 <just.viktor@gmail.com>
2022-07-20 16:23:26 -0400

    add js misc

--- src/Javascript/jq-misc-replacements.js
+++ src/Javascript/jq-misc-replacements.js
@@ -8,6 +8,13 @@ else document.addEventListener("DOMContentLoaded", callback);
 el.addEventListener("event-name", handler); // $el.on(eventName, eventHandler);
 el.removeEventListener("event-name", handler); // $el.off(eventName, eventHandler);
 
+// Modal inside Overlay click
+overlay.addEventListener('click', function (e) {
+  if (e.currentTarget === e.target) {
+    console.log('Close the modal');
+  }
+});
+
 // $(".element").trigger("myEvent");
 document.querySelector(".element").dispatchEvent(new Event("myEvent"));
 // CustomEvent
