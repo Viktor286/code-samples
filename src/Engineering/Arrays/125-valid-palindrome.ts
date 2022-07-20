3ff4ef831234c615279842b41f7fdf1baf4b2e2a
viktor286 <just.viktor@gmail.com>
2022-07-20 16:22:45 -0400

    add 125

---
+++ src/Engineering/Arrays/125-valid-palindrome.ts
@@ -0,0 +1,24 @@
+// 125. Valid Palindrome
+// A phrase is a palindrome if, after converting all
+// uppercase letters into lowercase letters and removing
+// all non-alphanumeric characters, it reads the same
+// forward and backward. Alphanumeric characters include
+// letters and numbers.
+
+function isPalindrome(s: string) {
+  const str = s.replaceAll(/[^A-Za-z0-9]/g, '').toLowerCase();
+  if (str === '') return true;
+
+  let l = 0;
+  let r = str.length - 1;
+
+  while (l < r) {
+    if (str[l] !== str[r]) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+
+  return true;
+}
