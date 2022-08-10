9edea319c5649bf30190bd0e6d6d18dc75fd5110
viktor286 <just.viktor@gmail.com>
2022-08-10 13:00:50 -0400

    add thunk middleware

---
+++ src/Javascript/redux-thunk-middleware.js
@@ -0,0 +1,24 @@
+// https://daveceddia.com/what-is-a-thunk/
+function createThunkMiddleware(extraArgument) {
+  return ({ dispatch, getState }) => next => action => {
+    // This gets called for every action you dispatch.
+    // If it's a function, call it.
+    if (typeof action === 'function') {
+      return action(dispatch, getState, extraArgument);
+    }
+
+    // Otherwise, just continue processing this action as usual
+    return next(action);
+  };
+}
+
+const thunk = createThunkMiddleware();
+thunk.withExtraArgument = createThunkMiddleware;
+
+export default thunk;
+
+// After you install redux-thunk in your project, and apply the middleware,
+// every action you dispatch will pass through this bit of code.
+// It calls actions that are functions (and returns whatever they return),
+// and otherwise passes the action along to then next middleware,
+// or to Redux itself (which is what next(action) does).
